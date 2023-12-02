6208b5fd0f0e744a813b4d413174fc46a422e31a
Petchctep <nattakit.somaram@gmail.com>
2023-12-03 04:17:13 +0700

    first time svelte/vite/bun

---
+++ bun-playground/myself/src/routes/profile/+page.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<title>Profile</title>
+	<meta name="profile" content="Personal Profile" />
+</svelte:head>
+
+<div>
+  <h1>Welcome to My Sandbox.</h1>
+    <details>
+      <summary>Lists of My Experience</summary>
+      <div>London Stock Exchange Group - Software Engineer</div>
+      <div>FINNOMENA - Software Engineer</div>
+      <div>NIPA Technology - Software Engineer</div>
+    </details>
+    <details>
+      <summary>Lists of My Sandbox You might found here</summary>
+      <span>None :P</span>
+    </details>
+    <details>
+      <summary>Lists of My Certification</summary>
+      <span>Azure Fundamental AZ-900</span>
+    </details>
+    <details>
+      <summary>Lists of My Courses I enrolled</summary>
+      <span>None.</span>
+    </details>
+</div>
---
+++ bun-playground/myself/src/routes/profile/design-system/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Design System</title>
+	<meta name="description" content="My Own Design System" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Here is my Own Design System I build.</h1>
+
+	<div>
+		<p>Template for For the PowerPoint, Google Slide Can be found here</p> <pre>My Google Slide Link with Shorted URL</pre>
+	</div>
+
+	<div>
+		<p>Design System for Development, Figma Can be found here</p> <pre>My Figma Link with Shorted-URL</pre>
+	</div>
+</div>
